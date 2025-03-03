fdcc1565ef150d54e360a5606cc2d541004fff7e
Nekane <montielarce9@gmail.com>
2025-03-03 17:01:33 +0100

    started

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+// Importamos la librería Fastify
+const Fastify = require('fastify');
+
+// Creamos el servidor Fastify
+const fastify = Fastify();
+
+// Configuramos una ruta simple
+fastify.get('/', async (request, reply) => {
+  console.log('Solicitud recibida');
+  return 'Hello from Fastify';
+});
+
+// Iniciamos el servidor en el puerto 3000
+fastify.listen({
+    port: 3000, 
+    host: '0.0.0.0'}, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Servidor Fastify corriendo en ${address}`);
+});
