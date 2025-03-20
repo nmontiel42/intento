0b7380c837071b141445c93ad35dbc1740cbe152
Antonio Torres Mora <atormora@gmail.com>
2025-03-20 14:54:02 +0100

    new changes to css

--- frontend/src/Chat.ts
+++ frontend/src/Chat.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let isSocketOpen = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -88,26 +87,3 @@ document.addEventListener("DOMContentLoaded", () => {
         chatMinimized = !chatMinimized;
     });
 });
-=======
-/* const chatContainer = document.getElementById("liveChatContainer") as HTMLElement;
-const chatBox = document.getElementById("chatBox") as HTMLElement;
-const chatInputContainer = document.getElementById("chatInputContainer") as HTMLElement;
-const toggleButton = document.getElementById("toggleChat") as HTMLButtonElement;
-
-let chatMinimized = true;
-
-toggleButton.addEventListener("click", () => {
-    if (chatMinimized) {
-        chatContainer.style.height = "50%";  // Restaura altura completa
-        chatBox.style.display = "flex";
-        chatInputContainer.style.display = "flex";
-        toggleButton.textContent = "−"; // Cambia icono
-    } else {
-        chatContainer.style.height = "50px";  // Solo muestra la cabecera
-        chatBox.style.display = "none";
-        chatInputContainer.style.display = "none";
-        toggleButton.textContent = "+"; // Cambia icono
-    }
-    chatMinimized = !chatMinimized;
-}); */
->>>>>>> main
